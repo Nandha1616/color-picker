bc27ea39e8f31ca4c01013b31e0a227702946883
Nandhakumar1602 <snivethanive12@gmail.com>
2025-09-17 17:36:39 +0530

    first-commit

---
+++ DOM.js
@@ -0,0 +1,54 @@
+
+// function add(){
+//     let num1=parseInt(document.getElementById("num1").value)
+//     let num2= parseInt(document.getElementById("num2").value)
+//     let num=num1+num2
+//     document.getElementById("para").innerHTML="Ans is"+num
+
+
+// }
+
+function para(){
+    // let num1=parseInt(document.getElementById("num1").value)
+    // let num2= parseInt(document.getElementById("num2").value)
+    // let num=num1-num2
+    document.getElementById("para").innerHTML="<p>Given that you are in Coimbatore and likely ride in mixed conditions (day, night, maybe rain), go for the clear visor with pins first. Then later you can get a smoke or tint version as backup or for day use.</p>"
+
+
+}
+function red() {
+    document.body.style.backgroundColor = "red";
+    document.getElementById("para").innerHTML = 
+        "You picked RED! <center><br><img src='https://images.unsplash.com/photo-1487019676766-812bfcd8029f?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' width='300'></center>";
+}
+function purple(){
+    
+    document. body. style. backgroundColor="purple"
+    document.getElementById("para").innerHTML ="You picked Purple! <center><br></br><img src='https://w0.peakpx.com/wallpaper/568/137/HD-wallpaper-purple-roses-purple-beautiful-satin-roses-flowers-nature.jpg' width='300'></center>";
+}
+function blue(){
+    
+    document. body. style. backgroundColor="blue"
+    document.getElementById("para").innerHTML =" You picked Blue!<center> <br></br><img src='https://m.media-amazon.com/images/I/71WjusTTbzL._AC_SL1500_.jpg' width='300'></center>";
+
+}
+function Green(){
+    
+    document. body. style. backgroundColor="green"
+    document.getElementById("para").innerHTML ="You picked Green! <center><br><img src='https://m.media-amazon.com/images/I/51PTaxoZ5qL._UF350,350_QL80_.jpg' width='300'></center>";
+
+}
+function Crimson(){
+    
+    document. body. style. backgroundColor="Crimson"
+    document.getElementById("para").innerHTML ="You picked Crimson! <center><br><img src='https://images.unsplash.com/photo-1697842609343-0a839fff5d09?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' width='300'></center>";
+
+}
+function gold(){
+    
+    document. body. style. backgroundColor="Gold"
+    document.getElementById("para").innerHTML ="You picked Gold!<center> <br><img src='https://img.freepik.com/free-photo/closeup-shot-yellow-rose-covered-with-dewdrops_181624-18291.jpg?semt=ais_incoming&w=740&q=80' width='300'></center>";
+
+}
+
+
